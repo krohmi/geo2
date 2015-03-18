a6d69c3fb9d25bae6f228045ec630228d5383522
krohmi <krohmi@o2.pl>
2015-03-18 09:29:23 +0100

    x1

---
+++ js/lokalizacja.js
@@ -0,0 +1,112 @@
+/* myLoc.js */
+
+var map = null;
+var ourCoords =  {
+	latitude: 51.099323,
+	longitude: 17.028845
+};
+
+window.onload = getMyLocation;
+
+function getMyLocation() {
+	if (navigator.geolocation) {
+
+		navigator.geolocation.getCurrentPosition(
+			displayLocation, 
+			displayError);
+	}
+	else {
+		alert("Nie można wyznaczyć Twojej lokalizacji");
+	}
+}
+
+function displayLocation(position) {
+	var latitude = position.coords.latitude;
+	var longitude = position.coords.longitude;
+
+	var div = document.getElementById("location");
+	div.innerHTML = "Twoja szerokość geograficzna: " + latitude + ", wysokość: " + longitude;
+
+	var km = computeDistance(position.coords, ourCoords);
+	var distance = document.getElementById("distance");
+	distance.innerHTML = "Jesteś " + km + " kilometrów od kolebki Wielkiego Trójkąta";
+
+	showMap(position.coords);
+}
+
+
+function computeDistance(startCoords, destCoords) {
+	var startLatRads = degreesToRadians(startCoords.latitude);
+	var startLongRads = degreesToRadians(startCoords.longitude);
+	var destLatRads = degreesToRadians(destCoords.latitude);
+	var destLongRads = degreesToRadians(destCoords.longitude);
+
+	var Radius = 6371; 
+	var distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) + 
+					Math.cos(startLatRads) * Math.cos(destLatRads) *
+					Math.cos(startLongRads - destLongRads)) * Radius;
+
+	return distance;
+}
+
+function degreesToRadians(degrees) {
+	radians = (degrees * Math.PI)/180;
+	return radians;
+}
+
+
+function showMap(coords) {
+	var googleLatAndLong = new google.maps.LatLng(coords.latitude, 
+												  coords.longitude);
+	var mapOptions = {
+		zoom: 10,
+		center: googleLatAndLong,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var mapDiv = document.getElementById("map");
+	map = new google.maps.Map(mapDiv, mapOptions);
+
+	// add the user marker
+	var title = "Your Location";
+	var content = "You are here: " + coords.latitude + ", " + coords.longitude;
+	addMarker(map, googleLatAndLong, title, content);
+
+}
+
+function addMarker(map, latlong, title, content) {
+	var markerOptions = {
+		position: latlong,
+		map: map,
+		title: title,
+		clickable: true
+	};
+	var marker = new google.maps.Marker(markerOptions);
+
+	var infoWindowOptions = {
+		content: content,
+		position: latlong
+	};
+
+	var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+
+	google.maps.event.addListener(marker, 'click', function() {
+		infoWindow.open(map);
+	});
+}
+
+
+function displayError(error) {
+	var errorTypes = {
+		0: "Unknown error",
+		1: "Permission denied",
+		2: "Position is not available",
+		3: "Request timeout"
+	};
+	var errorMessage = errorTypes[error.code];
+	if (error.code == 0 || error.code == 2) {
+		errorMessage = errorMessage + " " + error.message;
+	}
+	var div = document.getElementById("location");
+	div.innerHTML = errorMessage;
+}
+
